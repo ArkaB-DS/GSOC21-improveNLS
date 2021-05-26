fba0e372e8448e6098461ff986b7e5d3072750b7
john <nashjc@uottawa.ca>
2021-05-26 15:05:39 -0400

    more examples in "Variety"

--- ExDerivs.R
+++ ExDerivs.R
@@ -1,8 +1,9 @@
+## ExDerivs.R -- a file to explore different aspects of R in calculating Jacobians
+## This is an experimental file for learning. 2021-5-25
+
 rm(list=ls()) # clear the workspace (does not completely remove things, need to restart R sometimes)
 require(nlsr)
-
 printsum <- function(xx){ print(summary(xx))}
-
 traceval  <-  TRUE  # traceval set TRUE to debug or give full history
 
 # Data for Hobbs problem
@@ -13,25 +14,61 @@ tdat  <-  seq_along(ydat) # for testing
 # A simple starting vector -- must have named parameters for nlxb, nls, wrapnlsr.
 start1  <-  c(b1=1, b2=1, b3=1)
 eunsc  <-   y ~ b1/(1+b2*exp(-b3*tt))
+str(eunsc)
+ceunsc <- " y ~ b1/(1+b2*exp(-b3*tt))"
+str(ceunsc)
+print(as.formula(ceunsc)==eunsc)
 
 cat("LOCAL DATA IN DATA FRAMES\n")
 weeddata1  <-  data.frame(y=ydat, tt=tdat)
+weedenv <- list2env(weeddata1)
+## weedenv$b1 <- start1$b1
+## Error in start1$b1 : $ operator is invalid for atomic vectors
+weedenv$b1 <- start1[[1]]
+weedenv$b2 <- start1[[2]]
+weedenv$b3 <- start1[[3]]
+## b1<-1
+## b2<-1
+## b3<-1
+## y<-ydat
+## tt<-tdat
+## all.vars(eunsc)
+rexpr<-call("-",eunsc[[3]], eunsc[[2]])
+eval(rexpr, weedenv)
+ls.str(weedenv)
 
 
+
+names(start1)
+start1
+str(weeddata1)
+ldata<-list2env(as.list(start1),parent=weeddata1)
+?list2env
+ldata<-list2env(as.list(start1),envir=weeddata1)
+weedenv<-list2env(weeddata1)
+ldata<-list2env(as.list(start1),envir=weedenv)
+ldata
+ls.str(ldata)
+eval(rexpr,envir=ldata)
+
+
+mfeunsc<-model.frame(eunsc)
+str(mfeunsc)
 ## 2021-5-25 try derivatives
-
 library(numDeriv)
 print(eunsc)
 funsc <- model2rjfun(eunsc, start1, data=weeddata1) # from nlsr: creates a function
 print(funsc(start1))
+print(environment(funsc))
+print(ls.str(environment(funsc)))
+print(ls(environment(funsc)$data))
+eval(eunsc, environment(funsc))
 # following gives an error "theta should be of type character"
 nDfunsc<-try(numericDeriv(eunsc, start1, weeddata1))
 print(nDfunsc)
 print(start1)
 
 theta <- c("b1", "b2", "b3")
-weedenv <- list2env(weeddata1)
-weedenv$prms <- start1
 # Following gives error
 ## Error in numericDeriv(eunsc, theta, weedenv) : 
 ## 'language' object cannot be coerced to type 'double'
@@ -77,4 +114,5 @@ b2<-1
 b3<-1
 ## Error in numericDeriv(formunsc[3], theta) : attempt to apply non-function
 nDfunsc<-try(numericDeriv(formunsc[3], theta))
+
 
