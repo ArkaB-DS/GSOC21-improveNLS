bc5accfc4f21ac2b787fcb34ae6dae7f2b775049
john <nashjc@uottawa.ca>
2021-05-26 18:22:37 -0400

    Additions

--- ExDerivs.R
+++ ExDerivs.R
@@ -2,7 +2,11 @@
 ## This is an experimental file for learning. 2021-5-25
 
 rm(list=ls()) # clear the workspace (does not completely remove things, need to restart R sometimes)
-require(nlsr)
+
+library(nlsr)
+library(numDeriv)
+library(microbenchmark)
+
 printsum <- function(xx){ print(summary(xx))}
 traceval  <-  TRUE  # traceval set TRUE to debug or give full history
 
@@ -27,42 +31,50 @@ weedenv <- list2env(weeddata1)
 weedenv$b1 <- start1[[1]]
 weedenv$b2 <- start1[[2]]
 weedenv$b3 <- start1[[3]]
-## b1<-1
-## b2<-1
-## b3<-1
-## y<-ydat
-## tt<-tdat
-## all.vars(eunsc)
-rexpr<-call("-",eunsc[[3]], eunsc[[2]])
-eval(rexpr, weedenv)
 ls.str(weedenv)
+rexpr<-call("-",eunsc[[3]], eunsc[[2]])
+r0<-eval(rexpr, weedenv)
+cat("Sumsquares at 1,1,1 is ",sum(r0^2),"\n")
 
-
-
-names(start1)
-start1
-str(weeddata1)
-ldata<-list2env(as.list(start1),parent=weeddata1)
-?list2env
-ldata<-list2env(as.list(start1),envir=weeddata1)
-weedenv<-list2env(weeddata1)
+## Another way
 ldata<-list2env(as.list(start1),envir=weedenv)
 ldata
 ls.str(ldata)
 eval(rexpr,envir=ldata)
 
+## How to get a model frame? Do we need it?
 
+## Error in model.frame.default(eunsc, ldata) : variable lengths differ (found for 'b1')
+mfeunsc<-model.frame(eunsc, ldata)
+
+y<-ydat
+tt<-tdat
+b1<-1
+b2<-1
+b3<-1
+## Error in model.frame.default(eunsc) : variable lengths differ (found for 'b1')
 mfeunsc<-model.frame(eunsc)
+get_all_vars(eunsc)
+eval(eunsc, ldata)
 str(mfeunsc)
+
 ## 2021-5-25 try derivatives
-library(numDeriv)
 print(eunsc)
+## nlsr::model2rjfun forms a function with gradient (jacobian) attribute
 funsc <- model2rjfun(eunsc, start1, data=weeddata1) # from nlsr: creates a function
+tmodel2rjfun <- microbenchmark(model2rjfun(eunsc, start1, data=weeddata1))
+print(tmodel2rjfun)
+print(funsc)
 print(funsc(start1))
 print(environment(funsc))
 print(ls.str(environment(funsc)))
 print(ls(environment(funsc)$data))
 eval(eunsc, environment(funsc))
+vfunsc<-funsc(start1)
+print(vfunsc)
+tfunsc<-microbenchmark(funsc(start1))
+print(tfunsc)
+
 # following gives an error "theta should be of type character"
 nDfunsc<-try(numericDeriv(eunsc, start1, weeddata1))
 print(nDfunsc)
@@ -115,4 +127,21 @@ b3<-1
 ## Error in numericDeriv(formunsc[3], theta) : attempt to apply non-function
 nDfunsc<-try(numericDeriv(formunsc[3], theta))
 
+## seems to work
 
+ndeunsc<-numericDeriv(rexpr, theta, rho=weedenv)
+print(ndeunsc)
+print(sum(ndeunsc^2))
+tndeunsc<-microbenchmark(ndeunsc<-numericDeriv(rexpr, theta, rho=weedenv))
+print(tndeunsc)
+
+
+res0<-model2rjfun(eunsc, start1, data=weeddata1, jacobian=FALSE)
+res0(start1)
+jeunsc<-jacobian(res0, start1)
+jeunsc
+tjeunsc<-microbenchmark(jeunsc<-jacobian(res0, start1))
+print(tjeunsc)
+
+jeunsc-attr(vfunsc,"gradient")
+ndeunsc-attr(vfunsc,"gradient")
