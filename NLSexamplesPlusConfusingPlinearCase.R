d4bbad8fe66e98d247ff45b67aedeb68cabdd3fc
john <nashjc@uottawa.ca>
2021-05-31 15:57:18 -0400

    minor edit of DervisNLS; added example of plinear giving strange result.

---
+++ NLSexamplesPlusConfusingPlinearCase.R
@@ -0,0 +1,162 @@
+# NLSexamples.R
+require(graphics)
+
+DNase1 <- subset(DNase, Run == 1)
+
+## using a selfStart model
+fm1DNase1 <- nls(density ~ SSlogis(log(conc), Asym, xmid, scal), DNase1)
+summary(fm1DNase1)
+## the coefficients only:
+coef(fm1DNase1)
+## including their SE, etc:
+coef(summary(fm1DNase1))
+
+## using conditional linearity
+fm2DNase1 <- nls(density ~ 1/(1 + exp((xmid - log(conc))/scal)),
+                 data = DNase1,
+                 start = list(xmid = 0, scal = 1),
+                 algorithm = "plinear")
+summary(fm2DNase1)
+
+## using conditional linearity AND Asym -- why otherwise?? JN
+fm2aDNase1 <- nls(density ~ Asym/(1 + exp((xmid - log(conc))/scal)),
+                 data = DNase1,
+                 start = list(Asym=3, xmid = 0, scal = 1),
+                 algorithm = "plinear",
+                 trace = TRUE)
+summary(fm2aDNase1)
+
+## without conditional linearity
+fm3DNase1 <- nls(density ~ Asym/(1 + exp((xmid - log(conc))/scal)),
+                 data = DNase1,
+                 start = list(Asym = 3, xmid = 0, scal = 1))
+summary(fm3DNase1)
+
+## using Port's nl2sol algorithm
+fm4DNase1 <- nls(density ~ Asym/(1 + exp((xmid - log(conc))/scal)),
+                 data = DNase1,
+                 start = list(Asym = 3, xmid = 0, scal = 1),
+                 algorithm = "port")
+summary(fm4DNase1)
+
+## weighted nonlinear regression
+Treated <- Puromycin[Puromycin$state == "treated", ]
+weighted.MM <- function(resp, conc, Vm, K)
+{
+    ## Purpose: exactly as white book p. 451 -- RHS for nls()
+    ##  Weighted version of Michaelis-Menten model
+    ## ----------------------------------------------------------
+    ## Arguments: 'y', 'x' and the two parameters (see book)
+    ## ----------------------------------------------------------
+    ## Author: Martin Maechler, Date: 23 Mar 2001
+
+    pred <- (Vm * conc)/(K + conc)
+    (resp - pred) / sqrt(pred)
+}
+
+Pur.wt <- nls( ~ weighted.MM(rate, conc, Vm, K), data = Treated,
+              start = list(Vm = 200, K = 0.1))
+summary(Pur.wt)
+
+## Passing arguments using a list that can not be coerced to a data.frame
+lisTreat <- with(Treated,
+                 list(conc1 = conc[1], conc.1 = conc[-1], rate = rate))
+
+weighted.MM1 <- function(resp, conc1, conc.1, Vm, K)
+{
+     conc <- c(conc1, conc.1)
+     pred <- (Vm * conc)/(K + conc)
+    (resp - pred) / sqrt(pred)
+}
+Pur.wt1 <- nls( ~ weighted.MM1(rate, conc1, conc.1, Vm, K),
+               data = lisTreat, start = list(Vm = 200, K = 0.1))
+stopifnot(all.equal(coef(Pur.wt), coef(Pur.wt1)))
+
+## Chambers and Hastie (1992) Statistical Models in S  (p. 537):
+## If the value of the right side [of formula] has an attribute called
+## 'gradient' this should be a matrix with the number of rows equal
+## to the length of the response and one column for each parameter.
+
+weighted.MM.grad <- function(resp, conc1, conc.1, Vm, K)
+{
+  conc <- c(conc1, conc.1)
+
+  K.conc <- K+conc
+  dy.dV <- conc/K.conc
+  dy.dK <- -Vm*dy.dV/K.conc
+  pred <- Vm*dy.dV
+  pred.5 <- sqrt(pred)
+  dev <- (resp - pred) / pred.5
+  Ddev <- -0.5*(resp+pred)/(pred.5*pred)
+  attr(dev, "gradient") <- Ddev * cbind(Vm = dy.dV, K = dy.dK)
+  dev
+}
+
+Pur.wt.grad <- nls( ~ weighted.MM.grad(rate, conc1, conc.1, Vm, K),
+                   data = lisTreat, start = list(Vm = 200, K = 0.1))
+
+rbind(coef(Pur.wt), coef(Pur.wt1), coef(Pur.wt.grad))
+
+## In this example, there seems no advantage to providing the gradient.
+## In other cases, there might be.
+
+
+## The two examples below show that you can fit a model to
+## artificial data with noise but not to artificial data
+## without noise.
+x <- 1:10
+y <- 2*x + 3                            # perfect fit
+yeps <- y + rnorm(length(y), sd = 0.01) # added noise
+nls(yeps ~ a + b*x, start = list(a = 0.12345, b = 0.54321))
+## terminates in an error, because convergence cannot be confirmed:
+try(nls(y ~ a + b*x, start = list(a = 0.12345, b = 0.54321)))
+library(nlsr)
+try(nlxb(y ~ a + b*x, start = list(a = 0.12345, b = 0.54321)))
+
+
+## the nls() internal cheap guess for starting values can be sufficient:
+
+x <- -(1:100)/10
+y <- 100 + 10 * exp(x / 2) + rnorm(x)/10
+nlmod <- nls(y ~  Const + A * exp(B * x))
+summary(nlmod)
+
+plot(x,y, main = "nls(*), data, true function and fit, n=100")
+curve(100 + 10 * exp(x / 2), col = 4, add = TRUE)
+lines(x, predict(nlmod), col = 2)
+
+
+## The muscle dataset in MASS is from an experiment on muscle
+## contraction on 21 animals.  The observed variables are Strip
+## (identifier of muscle), Conc (Cacl concentration) and Length
+## (resulting length of muscle section).
+utils::data(muscle, package = "MASS")
+
+## The non linear model considered is
+##       Length = alpha + beta*exp(-Conc/theta) + error
+## where theta is constant but alpha and beta may vary with Strip.
+
+with(muscle, table(Strip)) # 2, 3 or 4 obs per strip
+
+## We first use the plinear algorithm to fit an overall model,
+## ignoring that alpha and beta might vary with Strip.
+
+## IGNORE_RDIFF_BEGIN
+musc.1 <- nls(Length ~ cbind(1, exp(-Conc/th)), muscle,
+              start = list(th = 1), algorithm = "plinear")
+summary(musc.1)
+## IGNORE_RDIFF_END
+
+## Then we use nls' indexing feature for parameters in non-linear
+## models to use the conventional algorithm to fit a model in which
+## alpha and beta vary with Strip.  The starting values are provided
+## by the previously fitted model.
+## Note that with indexed parameters, the starting values must be
+## given in a list (with names):
+b <- coef(musc.1)
+musc.2 <- nls(Length ~ a[Strip] + b[Strip]*exp(-Conc/th), muscle,
+              start = list(a = rep(b[2], 21), b = rep(b[3], 21), th = b[1]))
+
+summary(musc.2)
+
+
