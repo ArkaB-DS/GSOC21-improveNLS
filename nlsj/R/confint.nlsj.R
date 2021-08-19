3f1d04b6d027fbad79b64398f5a7ef5441ecaa1e
john <nashjc@uottawa.ca>
2021-08-19 18:12:57 -0400

    remove nlsj -- now on Github

--- nlsj/R/confint.nlsj.R
+++
@@ -1,8 +0,0 @@
-confint.nlsj <- function(object, parm, level = 0.95, ...)
-{
-    if(!requireNamespace("MASS", quietly = TRUE))
-        stop("package 'MASS' must be installed")
-    confint.nlsj <- get("confint.nls", asNamespace("MASS"), inherits = FALSE)
-    confint.nlsj(object, parm, level, ...)
-}
-
